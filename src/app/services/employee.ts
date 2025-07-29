e5cef013e27b44c8742d03a3377a08636744b67e
Adria <adria.jorda.m@gmail.com>
2025-07-29 18:25:59 +0200

    [add] creado el post del login, cuidado web no segura

--- src/app/services/employee.ts
+++ src/app/services/employee.ts
@@ -1,8 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Employee {
-  
+  constructor(private http: HttpClient ) {}
+
+  onLogin (obj: any) {
+    return this.http.post("https://freeapi.miniprojectideas.com/apiEmployeeLeave/Login", obj)
+  }
 }
