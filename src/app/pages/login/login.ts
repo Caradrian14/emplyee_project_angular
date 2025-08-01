c2683e9fbe0d1215a788727abe5a0a19ef59cfe2
Adria <adria.jorda.m@gmail.com>
2025-08-01 12:51:17 +0200

    [add] login ts y logica desarrollada

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { LoginModel } from '../../model/Employee.model';
+import { Employee } from '../../services/employee';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +11,24 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.scss'
 })
 export class Login {
-  loginObject: any = {
-    "emailId": "",
-    "passwrd": "1234"
+  loginObject: LoginModel = new LoginModel();
+
+  employeeService = inject(Employee)
+  router = inject(Router)
+  onLogin() {
+    this.employeeService.onLogin(this.loginObject).subscribe({
+      next:(result:any)=>{
+        if(result.result) {
+          alert("Login Success");
+          localStorage.setItem('leaveUser', JSON.stringify(result.data));
+          this.router.navigateByUrl("/dashboard")
+        } else {
+          alert(result.message)
+        }
+      },
+      error:()=> {
+        alert('Api error')
+      }
+    })
   }
 }
