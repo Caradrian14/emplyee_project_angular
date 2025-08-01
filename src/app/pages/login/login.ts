ce74ebd0fc520ee9fad37da681ce19c75b342194
Adria <adria.jorda.m@gmail.com>
2025-08-01 11:59:18 +0200

    [add] login simple y algo de estilos con objeto

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
-
+  loginObject: any = {
+    "emailId": "",
+    "passwrd": "1234"
+  }
 }
