b119c9b85bf4871d9789bab3712ea100ff210208
Adria <adria.jorda.m@gmail.com>
2025-08-03 11:54:44 +0200

    [add] nav y logoff

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
 export class Layout {
 
+  router = inject(Router)
+  logOff() {
+    localStorage.removeItem("leaveUser")
+    this.router.navigateByUrl("/login")
+  }
 }
