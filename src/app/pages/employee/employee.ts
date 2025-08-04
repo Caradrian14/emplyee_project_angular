cb3f062f5d3613941c497eff56a71559f2e9d054
Adria <adria.jorda.m@gmail.com>
2025-08-04 08:28:25 +0200

    [add] seleccion del buevo employee en employee ts del page

--- src/app/pages/employee/employee.ts
+++ src/app/pages/employee/employee.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { employeeService } from '../../services/employee';
 import { APIResponseModel, EmployeeList } from '../../model/Employee.model';
 
@@ -14,6 +14,8 @@ export class Employee implements OnInit{
   employeeService = inject(employeeService)
 
   employeeList: EmployeeList[]=[]
+
+  @ViewChild("newModal") newModal!: ElementRef;
 
   ngOnInit(): void {
     this.getEmployees();
