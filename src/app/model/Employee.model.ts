e4d9c067bf73daefd98ce703ffd36592b99d8fd0
Adria <adria.jorda.m@gmail.com>
2025-08-04 09:04:20 +0200

    [add] employee constructor en el modal

--- src/app/model/Employee.model.ts
+++ src/app/model/Employee.model.ts
@@ -22,5 +22,26 @@ export interface EmployeeList {
     "contactNo": string,
     "emailId": string,
     "role": string 
+}
 
+export class EmployeeModel {
+    employeeId:number
+    employeeName: string
+    contactNo: string
+    emailId: string
+    deptId: string
+    password:string
+    gender:string
+    role: string 
+
+    constructor() {
+        this.contactNo = '';
+        this.deptId = '';
+        this.emailId = '';
+        this.employeeId = 0;
+        this.gender = '';
+        this.employeeName = '';
+        this.password = '';
+        this.role = '';
+    }
 }
